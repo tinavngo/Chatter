9e6d474a74df5c80a1f8b88ac8755519e2a60de5
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-06-24 15:29:12 -0700

    Fetch and render data

--- App.js
+++ App.js
@@ -45,9 +45,9 @@ const Stack = createNativeStackNavigator();
         component={Start}
         />
         <Stack.Screen
-        name="Chat"
-        component={Chat}
-        />
+        name="Chat">
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView, FlatList, Text } from 'react-native';
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, orderBy, query } from "firebase/firestore";
 
-const Chat = ({route, navigation}) => {
+const Chat = ({route, navigation, db}) => {
+    const { userID, name, background } = route.params;
     const [messages, setMessages] = useState([]);
-    const { name, background } = route.params;
     const onSend = (newMessages) => {
-      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages)
-     );
+      addDoc(collection(db, "messages"), newMessages[0])
     };
 
     const renderBubble = (props) => {
@@ -27,41 +26,47 @@ const Chat = ({route, navigation}) => {
      );
     }
 
-    // set navigate options
+    // Set user name
     useEffect(() => {
-        navigation.setOptions({ title: name });
-      }, []);
-    
-    // set message options
+      navigation.setOptions({ title: name });
+  }, []);
+
+    // render Firebase data
     useEffect(() => {
-      setMessages([
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 2,
-          text: "This is a system message",
-          createdAt: new Date(),
-          system: true,
-        },
-      ]);
-     }, []);
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      const unsubMessages = onSnapshot(q, (documentSnapShot) => {
+        let newMessages = [];
+        documentSnapShot.forEach(doc => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis())
+          })
+        });
+        setMessages(newMessages);
+      });
+
+      return () => {
+        if (unsubMessages) unsubMessages();
+      }
+    }, []);
+
+
 
  return (
    <View style={[styles.container, { backgroundColor: background}]}>
+    <FlatList
+  data={messages}
+  renderItem={({ message }) =>
+    <Text>{message.text}</Text>}
+/>
     <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
         onSend={(messages) => onSend(messages)}
         user={{
-          _id: 1,
+          _id: userID,
+          name
         }}
       />
       {Platform.OS === "android" ? (
