527eb8b17862ca606a8c2169c96b849f7ad7f3fa
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-09-04 23:09:03 -0700

    Update App.js

--- App.js
+++ App.js
@@ -76,24 +76,5 @@ const Stack = createNativeStackNavigator();
   );
 }
 
-/* 
-  const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textInput: {
-    width: '88%',
-    borderWidth: 1,
-    height: 50,
-    padding: 10
-  },
-  textDisplay: {
-    height: 50,
-    lineHeight: 50
-  }
-});
-*/
 
 export default App;
